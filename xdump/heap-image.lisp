707f41d8e8ce1eea2886a7c3d8b83ab1ecebe4e6
Gary Byers <gb@clozure.com>
2008-02-03 12:32:24 +0000

    Change image-abi-version.  FASL version will change soon.

--- xdump/heap-image.lisp
+++ xdump/heap-image.lisp
@@ -96,7 +96,7 @@
 			   (+ 4095 (file-position f)))))
 
 
-(defparameter *image-abi-version* 1019)
+(defparameter *image-abi-version* 1020)
 
 (defun write-image-file (pathname image-base spaces &optional (abi-version *image-abi-version*))
   (target-setup-image-header-sizes)
