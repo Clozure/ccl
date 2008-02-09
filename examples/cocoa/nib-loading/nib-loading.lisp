d79e5ff9ecbb95a2a20135da69e2a80f6c237b72
mikel <mikel@clozure.com>
2008-02-09 23:27:31 +0000

    added nib-loading example

---
+++ examples/cocoa/nib-loading/nib-loading.lisp
@@ -0,0 +1,30 @@
+;;;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp; Base: 10; Package: cl-user -*-
+;;;; ***********************************************************************
+;;;; FILE IDENTIFICATION
+;;;;
+;;;; Name:          nib-loading.lisp
+;;;; Version:       0.1
+;;;; Project:       cocoa examples
+;;;; Purpose:       examples of loading nibs dynamically in the IDE
+;;;;
+;;;; ***********************************************************************
+
+(in-package :ccl)
+
+(defun load-nibfile (nib-path)
+  (let* ((appclass (#_NSClassFromString (%make-nsstring "NSApplication")))
+         (app (#/sharedApplication appclass))
+         (app-zone (#/zone app))
+         (nib-name (%make-nsstring (namestring nib-path)))
+         (toplevel-objects-array (#/arrayWithCapacity: (@class ns-mutable-array) 8))
+         (context (#/dictionaryWithObjectsAndKeys: (@class ns-mutable-dictionary)
+                                                   app #@"NSNibOwner" 
+                                                   toplevel-objects-array #@"NSNibTopLevelObjects"))
+         (load-succeeded-p (#/loadNibFile:externalNameTable:withZone: (@class ns-bundle)
+                                                                      nib-name context app-zone)))
+    (values load-succeeded-p context)))
+
+
+#|
+(ccl::load-nibfile "/usr/local/openmcl/trunk/source/examples/cocoa/nib-loading/hello.nib")
+|#
