ade972f832f3fb9c060867b41d7129e24c0f9bb1
R. Matthew Emerson <rme@clozure.com>
2008-07-19 02:55:15 +0000

    New for x8632 port.

---
+++ lib/x8632env.lisp
@@ -0,0 +1,74 @@
+; -*- Mode:Lisp; Package:CCL; -*-
+;;;
+;;;   Copyright (C) 2005 Clozure Associates
+;;;   This file is part of OpenMCL.  
+;;;
+;;;   OpenMCL is licensed under the terms of the Lisp Lesser GNU Public
+;;;   License , known as the LLGPL and distributed with OpenMCL as the
+;;;   file "LICENSE".  The LLGPL consists of a preamble and the LGPL,
+;;;   which is distributed with OpenMCL as the file "LGPL".  Where these
+;;;   conflict, the preamble takes precedence.  
+;;;
+;;;   OpenMCL is referenced in the preamble as the "LIBRARY."
+;;;
+;;;   The LLGPL is also available online at
+;;;   http://opensource.franz.com/preamble.html
+
+(in-package "CCL")
+
+(defconstant $numx8632saveregs 0)
+(defconstant $numx8632argregs 2)
+
+
+(defconstant x8632-nonvolatile-registers-mask 0)
+
+(defconstant x8632-arg-registers-mask
+  (logior (ash 1 x8632::arg_z)
+          (ash 1 x8632::arg_y)))
+  
+(defconstant x8632-temp-registers-mask
+  (logior (ash 1 x8632::temp0)
+	  (ash 1 x8632::temp1)))
+  
+(defconstant x8632-tagged-registers-mask
+  (logior x8632-temp-registers-mask
+          x8632-arg-registers-mask
+          x8632-nonvolatile-registers-mask))
+
+;;; Defined in x8664env.lisp, too
+#+x8632-target
+(defmacro make-mask (&rest weights)
+  `(logior ,@(mapcar #'(lambda (w) `(ash 1 ,w)) weights)))
+
+(defconstant x8632-temp-node-regs 
+  (make-mask x8632::temp0
+	     x8632::temp1
+             x8632::arg_y
+             x8632::arg_z))
+
+(defconstant x8632-nonvolatile-node-regs 0)
+
+(defconstant x8632-node-regs (logior x8632-temp-node-regs x8632-nonvolatile-node-regs))
+
+(defconstant x8632-imm-regs (make-mask
+                             x8632::imm0))
+
+;;; Fine if we assume SSE support;  not so hot when using x87
+(defconstant x8632-temp-fp-regs (make-mask x8632::fp0
+                                           x8632::fp1
+                                           x8632::fp2
+                                           x8632::fp3
+                                           x8632::fp4
+                                           x8632::fp5
+                                           x8632::fp6
+                                           x8632::fp7))
+                               
+
+
+(defconstant x8632-cr-fields (make-mask 0))
+
+;;; hmm.
+(defconstant $undo-x86-c-frame 16)
+
+
+(ccl::provide "X8632ENV")
