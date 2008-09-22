25f3b0801fc7e3db580b0e049703430707f7d87d
Gary Byers <gb@clozure.com>
2008-09-22 12:07:01 +0000

    bump (by a few) current, max image version number.  Will bump min when a few more changes made, including change in xloader

--- lisp-kernel/image.h
+++ lisp-kernel/image.h
@@ -90,7 +90,7 @@ load_openmcl_image(int, openmcl_image_file_header*);
 
 
 #define ABI_VERSION_MIN 1021
-#define ABI_VERSION_CURRENT 1021
-#define ABI_VERSION_MAX 1021
+#define ABI_VERSION_CURRENT 1025
+#define ABI_VERSION_MAX 1025
 
 #define NUM_IMAGE_SECTIONS 4    /* used to be 3 */
