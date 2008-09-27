c8ffcc23a31b719749e7f37f67d382965907f39c
Gary Byers <gb@clozure.com>
2008-09-27 06:50:06 +0000

    Load ffi-win32 if #+win32-target.

--- level-1/l1-boot-2.lisp
+++ level-1/l1-boot-2.lisp
@@ -267,6 +267,8 @@ present and false otherwise. This variable shouldn't be set by user code.")
       (bin-load-provide "FFI-WIN64" "ffi-win64")
       #+linuxx8632-target
       (bin-load-provide "FFI-LINUXX8632" "ffi-linuxx8632")
+      #+win32-target
+      (bin-load-provide "FFI-WIN32" "ffi-win32")
       
       (bin-load-provide "DB-IO" "db-io")
 
