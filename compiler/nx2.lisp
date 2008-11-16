bbb06599c3fb5d09ab33d43e5bdb595cf0d1a9a8
Gary Byers <gb@clozure.com>
2008-11-16 02:01:25 +0000

    New file, or maybe a very old one.  Empty, for now, and there may be some circular bootstrapping soon.

---
+++ compiler/nx2.lisp
@@ -0,0 +1,21 @@
+;;;-*-Mode: LISP; Package: ccl -*-
+;;;
+;;;   Copyright (C) 2008, Clozure Associates and contributors
+;;;   This file is part of OpenMCL.  
+;;;
+;;;   OpenMCL is licensed under the terms of the Lisp Lesser GNU Public
+;;;   License , known as the LLGPL and distributed with OpenMCL as the
+;;;   file "LICENSE".  The LLGPL consists of a preamble and the LGPL,
+;;;   which is distributed with OpenMCL as the file "LGPL".  Where these
+;;;   conflict, the preamble takes precedence.  
+;;;
+;;;   OpenMCL is referenced in the preamble as the "LIBRARY."
+;;;
+;;;   The LLGPL is also available online at
+;;;   http://opensource.franz.com/preamble.html
+
+;;; Shared compiler backend utilities and infrastructure.
+
+(in-package "CCL")
+
+
