1ce06049db95beea0e7c64334960b28ac87f5fb2
Gary Byers <gb@clozure.com>
2008-11-17 11:27:27 +0000

    Use 'jni-init' import to call JNI functions that clobber Mach exception ports on Darwin. (This still needs lots of work ...)

--- examples/jni.lisp
+++ examples/jni.lisp
@@ -21,9 +21,29 @@
 	      (pref initargs :<J>ava<VMI>nit<A>rgs.n<O>ptions) nargs
 	      (pref initargs :<J>ava<VMI>nit<A>rgs.options) options
 	      (pref initargs :<J>ava<VMI>nit<A>rgs.ignore<U>nrecognized) #$JNI_TRUE)
-	(let* ((result (#_JNI_CreateJavaVM  :monitor-exception-ports vm env initargs)))
+	(let* ((result
+                (ff-call (%kernel-import target::kernel-import-jvm-init)
+                         :address (foreign-symbol-address "JNI_CreateJavaVM")
+                         :address vm
+                         :address env
+                         :address initargs
+                         :int)))
 	  (if (>= result 0)
 	    (values (%get-ptr vm) (%get-ptr env))
 	    (error "Can't create Java VM: result = ~d" result)))))))
-	      
+
+; (%init-java-vm)
+
+(defun %attach-to-jvm (jvm)
+  (rlet ((envp :address +null-ptr+))
+    (let* ((result (ff-call (%kernel-import target::kernel-import-jvm-init)
+                            :address (pref jvm #>JavaVM.AttachCurrentThread)
+                            :address jvm
+                            :address envp
+                            :address +null-ptr+
+                            :int))
+           (env (pref envp :address)))
+      (if (eql 0 result)
+        env))))
+           
 	      
