25165754a4255116c1509aeb73b6715d24077562
Gary Byers <gb@clozure.com>
2008-12-17 21:14:09 +0000

    When creating a standalone application bundle (or rather when *COCOA-APPLICATION-INSTALL-ALTCONSOLE* is true), try to ensure that ccl:cocoa-ide;altconsole;AltConsole.app is up-to-date and copy it into the application bundle's Resources directory if so.

--- cocoa-ide/ide-bundle.lisp
+++ cocoa-ide/ide-bundle.lisp
@@ -12,6 +12,7 @@
 (defun create-ide-bundle (bundle-path &key (source "ccl:cocoa-ide;ide-contents;")
 				           (source-ignore '(".svn" "cvs" ".cvsignore"))
 					   (copy-headers *cocoa-application-copy-headers-p*)
+                                           (install-altconsole *cocoa-application-install-altconsole*)
 					   (if-exists :overwrite))
   ;; TODO: Right now if the bundle exists, we leave alone any files that we don't replace.
   ;; I'd like :if-exists :supersede mean to remove such files, for clean builds, but
@@ -40,7 +41,13 @@
 					    :directory (append (pathname-directory contents-dir)
 							       (cons "Resources" subdirs)))))
 	  (recursive-copy-directory ccl-headers dest-headers :if-exists if-exists :test #'ignore-test)))
+      (when install-altconsole
+        (install-altconsole bundle-path))
       ;; Is this necessary?
+      ;; At one point in the past, it was necessary for the bundle to
+      ;; contain an executable file whose name matched what was specified
+      ;; in its Info.plist file.  That executable file could be practically
+      ;; anything, as long as its executable bits were set.
       (let* ((image-name (ccl::standard-kernel-name))
 	     (ccl-image (make-pathname :name image-name :host "ccl"))
 	     (dest-image (make-pathname :name image-name
@@ -48,6 +55,34 @@
 	(ensure-directories-exist dest-image)
 	(copy-file ccl-image dest-image :if-exists :supersede :preserve-attributes t))
       (ccl::touch target-dir))))
+
+;;; This runs "make install" to generate
+;;; "ccl:cocoa-ide;altconsole;AltConsole.app",
+;;; then copies that application bundle into the "Resources" directory
+;;; of the target bundle.  It might be simpler to just have "make install"
+;;; put things in the right place, but "the right place" is likely to
+;;; be a pathname that contains a space. Quoting such a pathname -
+;;; and figuring out how to get make to do so - is left as an exercise.
+(defun install-altconsole (bundle-path)
+  (let* ((altconsole-path (merge-pathnames ";Contents;Resources;AltConsole.app;" bundle-path))
+         (build-directory "ccl:cocoa-ide;altconsole;")
+         (build-bundle-path "ccl:cocoa-ide;altconsole;AltConsole.app")
+         (make-output (make-string-output-stream))
+         (args `("-C" ,(native-translated-namestring build-directory) "install")))
+    (recursive-delete-directory altconsole-path :if-does-not-exist nil)
+    (unwind-protect
+         (multiple-value-bind (exit-status code)
+             (external-process-status
+              (run-program "make" args :output make-output :error make-output))
+           (unless (and (eq exit-status :exited) (zerop code))
+             (format t "~&'make install' of AltConsole.app failed:~&~a"
+                     (get-output-stream-string make-output))
+             (return-from install-altconsole nil)))
+      (close make-output))
+    ;;(ensure-directories-exist altconsole-path)
+    (recursive-copy-directory build-bundle-path altconsole-path)
+    (ccl::touch altconsole-path)
+    t))
 
 (progn
   (require "FAKE-CFBUNDLE-PATH")
