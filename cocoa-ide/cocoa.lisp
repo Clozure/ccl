a10f0218065c461c863542f77a45d9440c86d2ae
Gary Byers <gb@clozure.com>
2008-12-17 21:14:47 +0000

    Default *COCOA-APPLICATION-INSTALL-ALTCONSOLE* to nil here.

--- cocoa-ide/cocoa.lisp
+++ cocoa-ide/cocoa.lisp
@@ -2,5 +2,6 @@
 
 (defvar *cocoa-application-path* #+gz "ccl:GZ temp bundle.app;" #-gz "ccl:temp bundle.app;")
 (defvar *cocoa-application-copy-headers-p* nil)
+(defvar *cocoa-application-install-altconsole* nil)
 (load "ccl:cocoa-ide;defsystem.lisp")
 (load-ide)
