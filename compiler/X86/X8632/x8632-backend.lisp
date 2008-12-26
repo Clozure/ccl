a9ceac63dd4a03103923b048d46235aeb3f65889
R. Matthew Emerson <rme@clozure.com>
2008-12-26 17:52:34 +0000

    Finish unifiying x8632 FFI stuff.
    Most operating systems use the vanilla i386 ABI conventions.
    
    Darwin uses a special rule for returning small structures. Call the
    CALL-STRUCT-RETURN-BY-IMPLICIT-ARG-FUNCTION (from the FTD) in
    EXPAND-FF-CALL and GENERATE-CALLBACK-BINDINGS in order to tell when we
    need to deal with Darwin struct return.
    
    In GENERATE-CALLBACK-BINDINGS, return an 8th value (number of bytes of
    outgoing arguments pushed) for Windows's benefit;  other backends
    should just ignore it.

--- compiler/X86/X8632/x8632-backend.lisp
+++ compiler/X86/X8632/x8632-backend.lisp
@@ -284,21 +284,24 @@
 
 (pushnew *x8632-backend* *known-backends* :key #'backend-name)
 
-;;; FFI stuff.  Shared by several backends (Darwin notably excepted.)
+;;; FFI stuff.  Most operating systems use the same vanilla i386 ABI.
+;;; Darwin uses a variant that returns small (<= 64 bit) structures in
+;;; registers.  (There are some other Darwin exceptions too, but they
+;;; don't concern us here).
 
-;;; A returned structure is always passed as a "hidden" first argument.
+;;; A returned structure is passed as a hidden first argument.
 (defun x8632::record-type-returns-structure-as-first-arg (rtype)
   (declare (ignore rtype))
   t)
 
 ;;; All arguments are passed on the stack.
-;;;
-;;; (We don't support the __m64, __m128, __m128d, and __m128i types.)
-
 (defun x8632::expand-ff-call (callform args
 			      &key (arg-coerce #'null-coerce-foreign-arg)
 			      (result-coerce #'null-coerce-foreign-result))
   (let* ((result-type-spec (or (car (last args)) :void))
+	 (struct-by-value-p nil)
+	 (result-op nil)
+	 (result-temp nil)
 	 (result-form nil))
     (multiple-value-bind (result-type error)
 	(ignore-errors (parse-foreign-type result-type-spec))
@@ -307,11 +310,28 @@
 	(setq args (butlast args)))
       (collect ((argforms))
 	(when (typep result-type 'foreign-record-type)
-	  (setq result-form (pop args)
-		result-type *void-foreign-type*
-		result-type-spec :void)
-	  (argforms :address)
-	  (argforms result-form))
+	  (setq result-form (pop args))
+	  (if (funcall (ftd-ff-call-struct-return-by-implicit-arg-function
+			*target-ftd*) result-type)
+	    (progn
+	      (setq result-type *void-foreign-type*
+		    result-type-spec :void)
+	      (argforms :address)
+	      (argforms result-form))
+	    (progn
+	      ;; for Darwin
+	      (ecase (foreign-type-bits result-type)
+		(8 (setq result-type-spec :unsigned-byte
+			 result-op '%get-unsigned-byte))
+		(16 (setq result-type-spec :unsigned-halfword
+			  result-op '%get-unsigned-word))
+		(32 (setq result-type-spec :unsigned-fullword
+			  result-op '%get-unsigned-long))
+		(64 (setq result-type-spec :unsigned-doubleword
+			  result-op '%%get-unsigned-longlong)))
+	      (setq result-type (parse-foreign-type result-type-spec))
+	      (setq result-temp (gensym))
+	      (setq struct-by-value-p t))))
 	(unless (evenp (length args))
 	  (error "~s should be an even-length list of alternating foreign types and values" args))
 	(do* ((args args (cddr args)))
@@ -335,41 +355,55 @@
 			arg-type-spec (foreign-type-to-representation-type
 				       ftype)
 			bits (ensure-foreign-type-bits ftype)))
-                    (if (typep ftype 'foreign-record-type)
-                      (argforms (ceiling bits 32))
-                      (argforms (foreign-type-to-representation-type ftype)))
+		(if (typep ftype 'foreign-record-type)
+		  (argforms (ceiling bits 32))
+		  (argforms (foreign-type-to-representation-type ftype)))
 		(argforms (funcall arg-coerce arg-type-spec arg-value-form))))))
-	  (argforms (foreign-type-to-representation-type result-type))
-	  (funcall result-coerce result-type-spec
-		   `(,@callform ,@(argforms)))))))
+	(argforms (foreign-type-to-representation-type result-type))
+	(let* ((call (funcall result-coerce result-type-spec
+			      `(,@callform ,@(argforms)))))
+	  (if struct-by-value-p
+	    `(let* ((,result-temp (%null-ptr)))
+	       (declare (dynamic-extent ,result-temp)
+			(type macptr ,result-temp))
+	       (%setf-macptr ,result-temp ,result-form)
+	       (setf (,result-op ,result-temp 0)
+		     ,call))
+	    call))))))
 
-
-;;; Return 7 values:
+;;; Return 8 values:
 ;;; A list of RLET bindings
 ;;; A list of LET* bindings
 ;;; A list of DYNAMIC-EXTENT declarations for the LET* bindings
 ;;; A list of initializaton forms for (some) structure args (not used on x8632)
 ;;; A FOREIGN-TYPE representing the "actual" return type.
 ;;; A form which can be used to initialize FP-ARGS-PTR, relative
-;;;  to STACK-PTR.  (This is unused on x8632.)
+;;;  to STACK-PTR.  (not used on x8632)
 ;;; The byte offset of the foreign return address, relative to STACK-PTR
+;;; The number of argument bytes pushed on the stack by the caller, or NIL
+;;;  if this can't be determined. (Only meaningful on Windows.)
 
 (defun x8632::generate-callback-bindings (stack-ptr fp-args-ptr argvars
 					  argspecs result-spec
 					  struct-result-name)
   (declare (ignore fp-args-ptr))
   (collect ((lets)
+	    (rlets)
 	    (dynamic-extent-names))
     (let* ((rtype (parse-foreign-type result-spec)))
       (when (typep rtype 'foreign-record-type)
-	(setq argvars (cons struct-result-name argvars)
-	      argspecs (cons :address argspecs)
-	      rtype *void-foreign-type*))
+	(if (funcall (ftd-ff-call-struct-return-by-implicit-arg-function
+		      *target-ftd*) rtype)
+	  (setq argvars (cons struct-result-name argvars)
+		argspecs (cons :address argspecs)
+		rtype *void-foreign-type*)
+	  (rlets (list struct-result-name (foreign-record-type-name rtype)))))
       (do* ((argvars argvars (cdr argvars))
 	    (argspecs argspecs (cdr argspecs))
 	    (offset 8))
 	   ((null argvars)
-	    (values nil (lets) (dynamic-extent-names) nil rtype nil 4))
+	    (values (rlets) (lets) (dynamic-extent-names) nil rtype nil 4
+		    (- offset 8)))
 	(let* ((name (car argvars))
 	       (spec (car argspecs))
 	       (argtype (parse-foreign-type spec))
@@ -405,10 +439,23 @@
 
 (defun x8632::generate-callback-return-value (stack-ptr fp-args-ptr result
 					      return-type struct-return-arg)
-  (declare (ignore fp-args-ptr struct-return-arg))
+  (declare (ignore fp-args-ptr))
   (unless (eq return-type *void-foreign-type*)
-    (let* ((return-type-keyword (foreign-type-to-representation-type
-				 return-type)))
+    (if (typep return-type 'foreign-record-type)
+      ;; On non-Darwin systems, the result type would have been mapped
+      ;; to :VOID.  On Darwin, small (<= 64 bits) structs are returned
+      ;; by value.
+      (ecase (ensure-foreign-type-bits return-type)
+	(8 `(setf (%get-unsigned-byte ,stack-ptr -8)
+		  (%get-unsigned-byte ,struct-return-arg 0)))
+	(16 `(setf (%get-unsigned-word ,stack-ptr -8)
+		   (%get-unsigned-word ,struct-return-arg 0)))
+	(32 `(setf (%get-unsigned-long ,stack-ptr -8)
+		   (%get-unsigned-long ,struct-return-arg 0)))
+	(64 `(setf (%%get-unsigned-longlong ,stack-ptr -8)
+	       (%%get-unsigned-longlong ,struct-return-arg 0))))
+      (let* ((return-type-keyword (foreign-type-to-representation-type
+				   return-type)))
         (collect ((forms))
           (forms 'progn)
           (case return-type-keyword
@@ -427,7 +474,7 @@
                      (:unsigned-fullword '%get-unsigned-long)
                      (t '%get-signed-long)
                      ) ,stack-ptr -8) ,result))
-          (forms)))))
+          (forms))))))
 
 
 
