2253ae9a4fcc67e319db703ad7d20aaaeece868f
R. Matthew Emerson <rme@clozure.com>
2008-12-27 03:50:43 +0000

    Looks the C compiler on FreeBSD returns small structs in registers. It appears to be the same technique as used on Darwin.
    http://gcc.gnu.org/onlinedocs/gcc/Incompatibilities.html

--- lib/ffi-freebsdx8632.lisp
+++ lib/ffi-freebsdx8632.lisp
@@ -1,7 +1,17 @@
 (in-package "CCL")
 
+;;; On FreeBSD, the C compiler returns small structures in registers
+;;; (just like on Darwin, apparently).
 (defun x86-freebsd32::record-type-returns-structure-as-first-arg (rtype)
-  (x8632::record-type-returns-structure-as-first-arg rtype))
+  (when (and rtype
+	     (not (typep rtype 'unsigned-byte))
+	     (not (member rtype *foreign-representation-type-keywords*
+			  :test #'eq)))
+    (let* ((ftype (if (typep rtype 'foreign-type)
+		    rtype
+		    (parse-foreign-type rtype)))
+	   (nbits (ensure-foreign-type-bits ftype)))
+      (not (member nbits '(8 16 32 64))))))
 
 (defun x86-freebsd32::expand-ff-call (callform args &key (arg-coerce #'null-coerce-foreign-arg) (result-coerce #'null-coerce-foreign-result))
   (x8632::expand-ff-call callform args :arg-coerce arg-coerce :result-coerce result-coerce))
