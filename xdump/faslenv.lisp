6c35c8962bd1731767810c2a441fece1129c2090
Gary Byers <gb@clozure.com>
2009-07-09 09:43:49 +0000

    Really bump max fasl version.

--- xdump/faslenv.lisp
+++ xdump/faslenv.lisp
@@ -42,7 +42,7 @@
 (defconstant $fasl-epush-bit 7)
 (defconstant $fasl-file-id #xff00)
 (defconstant $fasl-file-id1 #xff01)
-(defconstant $fasl-vers #x54)
+(defconstant $fasl-vers #x57)
 (defconstant $fasl-min-vers #x54)
 (defconstant $faslend #xff)
 (defconstant $fasl-buf-len 2048)
