152b0981b3a17c360cee15a5108469145dbb63b0
Gail Zacharias <gz@clozure.com>
2010-07-14 19:23:00 +0000

    function inspector: show lfun bits and lfun info

--- lib/describe.lisp
+++ lib/describe.lisp
@@ -1229,6 +1229,8 @@
                                               (format nil "Arglist (~(~a~))" type)
                                               "Arglist unknown")))
                                  (list arglist label (if type :colon '(:comment (:plain)))))))
+                       (list (list (ccl::lfun-bits o) "Bits" :colon))
+                       (list (list (ccl::%lfun-info o) "Plist" :colon))
                        (when doc (list (list (substitute #\space #\newline doc) "Documentation" :colon)))
                        (when sn (list (list sn "Source Location" :colon))))))
     (setf (slot-value f 'header-lines) lines)))
