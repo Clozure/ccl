b473906759614072daf80d01c100a94d3066f9a6
arthur <arthur@clozure.com>
2010-08-26 08:54:04 +0000

    add ability for button actions to use shift-key-p and friends

--- examples/cocoa/easygui/action-targets.lisp
+++ examples/cocoa/easygui/action-targets.lisp
@@ -4,14 +4,18 @@
 ;;; action/targets
 
 (defclass generic-easygui-target (ns:ns-object)
-     ((handler :initarg :handler :reader target-handler))
+     ((handler :initarg :handler :reader target-handler)
+      (shooter :initarg :shooter :reader target-shooter))
   (:metaclass ns:+ns-object))
 
 (objc:defmethod (#/activateAction :void) ((self generic-easygui-target))
-  (funcall (target-handler self)))
+  (let* ((cell (#/selectedCell (target-shooter self)))
+         (responds (#/respondsToSelector: cell (@selector #/mouseDownFlags)))
+         (*modifier-key-pattern* (if responds (#/mouseDownFlags cell) 0)))
+    (funcall (target-handler self))))
 
 (defmethod (setf action) (handler (view view))
   (let ((target (make-instance 'generic-easygui-target
-                   :handler handler)))
+                   :handler handler :shooter (cocoa-ref view))))
     (#/setTarget: (cocoa-ref view) target)
     (#/setAction: (cocoa-ref view) (@selector #/activateAction))))
