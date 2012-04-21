8df4c43e6c45a17506595fa07913e23ba90de993
Gary Byers <gb@clozure.com>
2012-04-21 00:57:19 +0000

    Set FLOAT_ABI in an included file ('float_abi.mk').  Still defaults to softfp, but a little easier to change persistently.
    'clean' and 'strip' targets reference ../../armcl, not ../../ppccl ...

---
+++ lisp-kernel/linuxarm/float_abi.mk
@@ -0,0 +1,5 @@
+# This file should define FLOAT_ABI as one of "softfp" or "hard".
+# If you change this, do 'make clean' to remove any object files
+# compiled for the other ABI.
+FLOAT_ABI = softfp
+#FLOAT_ABI = hard
