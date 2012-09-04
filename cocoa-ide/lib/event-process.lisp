60cf0c9d9414354bcce39aa682e4c73fa4cefb02
Gary Byers <gb@clozure.com>
2012-09-04 09:19:25 +0000

    Decide whether to use the libdispatch-based mechanism or one based on #/performSelectorOnMainThread:... in the implementation of QUEUE-FOR-EVENT-PROCESS and CALL-IN-EVENT-PROCESS based on the runtime value of GUI::*INTERRUPT-VIA-DISPATCH-QUEUE*.  Default to using the new mechanism, for now.

--- cocoa-ide/lib/event-process.lisp
+++ cocoa-ide/lib/event-process.lisp
@@ -7,17 +7,35 @@
 ;;; of speaking of the "main thread" or the "event thread", we will
 ;;; use the term "event process".
 
-;;; These next two functions use libdispatch (aka Grand Central
-;;; Dispatch) to invoke functions on the main thread.  Note that the
-;;; functions will hang if there is not an active run loop on the main
-;;; thread.
+
+(defvar gui::*interrupt-via-dispatch-queue* nil)
+
+(defstatic *interrupt-id-map* (make-id-map))
+
+(objc:defmethod (#/lispInterrupt: :void) ((self ns:ns-application)
+                                          id)
+  (funcall (id-map-free-object *interrupt-id-map* (#/intValue (#/autorelease id)))))
+
+(defun %interrupt-event-process (f wait)
+  (#/performSelectorOnMainThread:withObject:waitUntilDone:
+   gui::*NSApp*
+   (objc:@selector #/lispInterrupt:)
+   (make-instance 'ns:ns-number :with-int (assign-id-map-id *interrupt-id-map* f))
+   wait))
+  
+;;; These next two functions conditionally use libdispatch (aka Grand
+;;; Central Dispatch) to invoke functions on the main thread.  Note
+;;; that the functions will hang if there is not an active run loop on
+;;; the main thread.
 
 (defun queue-for-event-process (f)
   "Queue the zero-argument function F for asynchronous execution in
 the event process."
   (if (eq *current-process* *initial-process*)
     (funcall f)
-    (dispatch-async *dispatch-main-queue* f)))
+    (if gui::*interrupt-via-dispatch-queue*
+      (dispatch-async *dispatch-main-queue* f)
+      (%interrupt-event-process f nil))))
 
 (defun call-in-event-process (f)
   "Invoke the zero-argument function F in the event process, wait for
@@ -28,7 +46,9 @@ it to finish, and return whatever values F returns."
       (flet ((wrapper ()
                (setq return-values (multiple-value-list (funcall f)))))
         (declare (dynamic-extent #'wrapper))
-        (dispatch-sync *dispatch-main-queue* #'wrapper)
+        (if gui::*interrupt-via-dispatch-queue*
+          (dispatch-sync *dispatch-main-queue* #'wrapper)
+          (%interrupt-event-process #'wrapper t))
         (apply #'values return-values)))))
 
 
