c22331e23fd3ab219ba45dfaea691d022c6a5046
Gary Byers <gb@clozure.com>
2013-02-15 07:56:37 +0000

    REBUILD-CCL: if building the kernel, do so after rebuilding the bootstrapping image.  (This can make it simpler to generate images whose svn revision doesn't contain an "M" suffix in some cases.)

--- lib/compile-ccl.lisp
+++ lib/compile-ccl.lisp
@@ -590,7 +590,6 @@ not runtime errors reported by a successfully created process."
       (setq full t))
     (when full
       (setq clean t kernel t reload t))
-
     (when update
       (multiple-value-bind (changed conflicts new-binaries)
 	  (update-ccl :verbose (not (eq update :quiet)))
@@ -620,6 +619,9 @@ the lisp and run REBUILD-CCL again.")
                                            :type (pathname-type *.fasl-pathname*))
                             "ccl:**;")))
                  (delete-file f)))
+             (with-global-optimization-settings ()
+               (compile-ccl (not (null force)))
+               (if force (xload-level-0 :force) (xload-level-0)))
              (when kernel
                (when (or clean force)
                  ;; Do a "make -k clean".
@@ -631,60 +633,57 @@ the lisp and run REBUILD-CCL again.")
                                     "clean")))
                (format t "~&;Building lisp-kernel ...")
                (with-output-to-string (s)
-                                      (let* ((proc (run-program (make-program)
-                                                                (list "-k" "-C" 
-                                                                      (format nil "lisp-kernel/~a"
-                                                                              (kernel-build-directory))
-                                                                      "-j"
+                 (let* ((proc (run-program (make-program)
+                                           (list "-k" "-C" 
+                                                 (format nil "lisp-kernel/~a"
+                                                         (kernel-build-directory))
+                                                 "-j"
                                                             
-                                                                      (format nil "~d" (1+ (cpu-count))))
-                                                                :output s
-                                                                :error :output)))
-                                        (multiple-value-bind (status exit-code)
-                                            (external-process-status proc)
-                                          (if (and (eq :exited status) (zerop exit-code))
-                                            (progn
-                                              (format t "~&;Kernel built successfully.")
-                                              (when verbose
-                                                (format t "~&;kernel build output:~%~a"
-                                                        (get-output-stream-string s)))
-                                              (sleep 1))
-                                            (error "Error(s) during kernel compilation.~%~a"
-                                                   (or
-                                                    (describe-external-process-failure
-                                                     proc
-                                                     "Developer tools may not be installed correctly.")
-                                                    (get-output-stream-string s))))))))
-             (with-global-optimization-settings ()
-               (compile-ccl (not (null force)))
-               (if force (xload-level-0 :force) (xload-level-0)))
+                                                 (format nil "~d" (1+ (cpu-count))))
+                                           :output s
+                                           :error :output)))
+                   (multiple-value-bind (status exit-code)
+                       (external-process-status proc)
+                     (if (and (eq :exited status) (zerop exit-code))
+                       (progn
+                         (format t "~&;Kernel built successfully.")
+                         (when verbose
+                           (format t "~&;kernel build output:~%~a"
+                                   (get-output-stream-string s)))
+                         (sleep 1))
+                       (error "Error(s) during kernel compilation.~%~a"
+                              (or
+                               (describe-external-process-failure
+                                proc
+                                "Developer tools may not be installed correctly.")
+                               (get-output-stream-string s))))))))
              (when reload
                (with-input-from-string (cmd (format nil
-                                                    "(save-application ~s)"
-                                                    (standard-image-name)))
+                                              "(save-application ~s)"
+                                              (standard-image-name)))
                  (with-output-to-string (output)
-                                        (multiple-value-bind (status exit-code)
-                                            (external-process-status
-                                             (run-program
-                                              (format nil "./~a" (standard-kernel-name))
-                                              (list* "--image-name" (standard-boot-image-name)
-                                                     "--batch"
-                                                     reload-arguments)
-                                              :input cmd
-                                              :output output
-                                              :error output))
-                                          (if (and (eq status :exited)
-                                                   (eql exit-code 0))
-                                            (progn
-                                              (format t "~&;Wrote heap image: ~s"
-                                                      (truename (format nil "ccl:~a"
-                                                                        (standard-image-name))))
-                                              (when verbose
-                                                (format t "~&;Reload heap image output:~%~a"
-                                                        (get-output-stream-string output))))
-                                            (error "Errors (~s ~s) reloading boot image:~&~a"
-                                                   status exit-code
-                                                   (get-output-stream-string output)))))))
+                   (multiple-value-bind (status exit-code)
+                       (external-process-status
+                        (run-program
+                         (format nil "./~a" (standard-kernel-name))
+                         (list* "--image-name" (standard-boot-image-name)
+                                "--batch"
+                                reload-arguments)
+                         :input cmd
+                         :output output
+                         :error output))
+                     (if (and (eq status :exited)
+                              (eql exit-code 0))
+                       (progn
+                         (format t "~&;Wrote heap image: ~s"
+                                 (truename (format nil "ccl:~a"
+                                                   (standard-image-name))))
+                         (when verbose
+                           (format t "~&;Reload heap image output:~%~a"
+                                   (get-output-stream-string output))))
+                       (error "Errors (~s ~s) reloading boot image:~&~a"
+                              status exit-code
+                              (get-output-stream-string output)))))))
              (when exit
                (quit)))
         (setf (current-directory) cd)))))
