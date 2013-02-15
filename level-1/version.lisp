09de744b32b5015364695bd13aa64e4b0bb1567f
Gary Byers <gb@clozure.com>
2013-02-15 08:03:37 +0000

    rc2

--- level-1/version.lisp
+++ level-1/version.lisp
@@ -19,7 +19,7 @@
 
 (defparameter *openmcl-major-version* 1)
 (defparameter *openmcl-minor-version* 9)
-(defparameter *openmcl-revision* "rc1")
+(defparameter *openmcl-revision* "rc2")
 ;;; May be set by xload-level-0
 (defvar *openmcl-svn-revision* nil)
 (defparameter *openmcl-dev-level* nil)
