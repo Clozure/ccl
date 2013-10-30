9e5fd735b80ed0e5c55c2d1900a0bee9697d84bd
Gary Byers <gb@clozure.com>
2013-10-30 05:44:42 +0000

    In the Makefile, set FLOAT_ABI_OPTION to the empty string; pass the value of that variable to the C compiler/assembler/linker, letting them default -mfloat-abi to (hopefully) an appropriate default.
    Allow float_abi.mk to override that default.  (In most cases, it
    should only be necessary to change that file if the toolchain default
    is inappropriate for the target system.)

--- lisp-kernel/linuxarm/float_abi.mk
+++ lisp-kernel/linuxarm/float_abi.mk
@@ -1,5 +1,7 @@
-# This file should define FLOAT_ABI as one of "softfp" or "hard".
+# By default, use the toolchain default -mfloat-abi option
+FLOAT_ABI_OPTION =
 # If you change this, do 'make clean' to remove any object files
 # compiled for the other ABI.
-FLOAT_ABI = softfp
-#FLOAT_ABI = hard
+#FLOAT_ABI_OPTION = -mfloat-abi=softfp
+#FLOAT_ABI_OPTION = -mfloat-abi=hard
+
