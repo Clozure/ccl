b5993cb9e15f9449cc49803d58ee4ebcfa637a08
svspire <svspire@clozure.com>
2014-03-12 20:49:31 +0000

    New macro with-general-cfurl. Shouldn't affect anything that currently uses with-cfurl.

--- cocoa-ide/lib/cf-utils.lisp
+++ cocoa-ide/lib/cf-utils.lisp
@@ -1,6 +1,6 @@
 (in-package "CCL")
 
-(export '(with-cfstring %get-cfstring with-cfurl))
+(export '(with-cfstring %get-cfstring with-cfurl with-general-cfurl))
 
 ;;; We could use something like ccl:with-pointer-to-ivector to get a
 ;;; pointer to the lisp string's underlying vector of UTF-32 code
@@ -43,7 +43,23 @@
       (#_CFURLCreateFromFileSystemRepresentation +null-ptr+ s noctets dir-p))))
 
 (defmacro with-cfurl ((sym thing) &body body)
+  "Thing must represent a file here, not a more general type of URL."
   `(let ((,sym (%make-cfurl ,thing)))
+     (unwind-protect
+	  (progn ,@body)
+       (unless (%null-ptr-p ,sym)
+	 (external-call "CFRelease" :address ,sym :void)))))
+
+(defmethod %make-general-cfurl ((pathname pathname))
+  (%make-cfurl pathname))
+
+(defmethod %make-general-cfurl ((string string))
+  (with-cfstring (s string)
+      (#_CFURLCreateWithString +null-ptr+ s +null-ptr+)))
+
+(defmacro with-general-cfurl ((sym thing) &body body)
+  "Thing may repesent a file or a more general URL. Its type determines the conversion."
+  `(let ((,sym (%make-general-cfurl ,thing)))
      (unwind-protect
 	  (progn ,@body)
        (unless (%null-ptr-p ,sym)
@@ -52,25 +68,23 @@
 (defun %open-url-in-browser (cfurl)
   (#_LSOpenCFURLRef cfurl +null-ptr+))
 
-(defun open-url-in-browser (url-string)
+(defun open-url-in-browser (url)
   "Open the given absolute url with the current browser of choice"
-  (let ((result (with-cfstring (s url-string)
-		  (let ((u (#_CFURLCreateWithString +null-ptr+ s +null-ptr+)))
-		    (when (%null-ptr-p u)
-		      (error "can't make a URL from ~s" url-string))
-		    (prog1
-			(%open-url-in-browser u)
-		      (#_CFRelease u))))))
-    (if (= result #$fnfErr)
-      ;; if it was a file: URL and resulted in file-not-found, try
-      ;; again, removing "Volumes/" from the filename. Sheesh.
-      (let ((pos (search "Volumes/" url-string :test #'string-equal)))
+  (let ((result (with-general-cfurl (my-url url)
+                  (%open-url-in-browser my-url))))
+    (if (and result (= result #$fnfErr))
+      ; if it was a file: URL and resulted in file-not-found, try again, removing
+      ;   "Volumes/" from the filename. Sheesh.
+      (let ((pos (search "Volumes/" url :test #'string-equal)))
         (if pos
-	  (open-url-in-browser (concatenate 'string
-					    (subseq url-string 0 pos)
-					    (subseq url-string (+ pos 8))))))
+          (open-url-in-browser (concatenate 'string
+					    (subseq url 0 pos)
+					    (subseq url (+ pos 8))))
+          result))
       result)))
 
 ; (ccl::open-url-in-browser "http://www.google.com/")
 ; (ccl::open-url-in-browser "file:///Applications/ccl/trunk/ccl/doc/ccl-documentation.html")
+; (ccl::open-url-in-browser "file:///Volumes/Volumes/MyMac/Applications/ccl/trunk/ccl/doc/ccl-documentation.html")
+; (ccl::open-url-in-browser "file:///Volumes/MyMac/Applications/ccl/trunk/ccl/doc/ccl-documentation.html")
 
