00fe8935535be6006d4eaa8d5badf668e5974926
Gary Byers <gb@clozure.com>
2016-02-20 13:40:56 +0000

    Add a compiler-macro on LAST.  Fixes ticket:1338 in the trunk.

--- compiler/optimizers.lisp
+++ compiler/optimizers.lisp
@@ -595,6 +595,16 @@
     (symbol t)
     (string t)))
 
+(define-compiler-macro last (&whole call L &optional (n 1 n-p))
+  (if (and n-p (not (eql n 1)))
+    call
+    (let* ((checked (gensym))
+           (returned (gensym)))
+      `(do* ((,checked ,L (cdr ,checked))
+              (,returned ,checked))
+        ((atom ,checked) ,returned)
+        (setq ,returned ,checked)))))
+
 (define-compiler-macro ldb (&whole call &environment env byte integer)
   (cond ((and (integerp byte) (> byte 0))
          (let ((size (byte-size byte))
