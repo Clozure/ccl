5f55eae41d4a3e2d262a7dc76e153dc7f887b92f
Bo Yao <icerove@gmail.com>
2018-07-15 12:06:13 +0800

    update the batch files

--- lisp-kernel/win64/m4as.bat
+++ lisp-kernel/win64/m4as.bat
@@ -4,8 +4,9 @@ set m4=C:\msys64\usr\bin\m4
 set as=C:\msys64\mingw64\bin\as
 
 if "%~1" neq "" (
-    %m4% -DWIN_64 -DWINDOWS -DX86 -DX8664 -DHAVE_TLS -DEMUTLS -DTCR_IN_GPR -I .. %~1 m4macros.m4 >  %~1.asm
-    %as% %~1.asm -g --64 -o %~n1.obj
+    %m4% -DWIN_64 -DWINDOWS -DX86 -DX8664 -DHAVE_TLS -DEMUTLS -DTCR_IN_GPR -I .. %~n1.s m4macros.m4 >  %~n1.asm
+    %as% %~n1.asm -g --64 -o %~n1.obj
+	del %~n1.asm
 ) else (
-    echo Usage: m4yasm.bat filename
+    echo Usage: m4as.bat filename
 )
--- lisp-kernel/win64/m4yasm.bat
+++ lisp-kernel/win64/m4yasm.bat
@@ -4,8 +4,9 @@ set m4=C:\msys64\usr\bin\m4
 set yasm=C:\msys64\usr\bin\yasm
 
 if "%~1" neq "" (
-    %m4% -DWIN_64 -DWINDOWS -DX86 -DX8664 -DHAVE_TLS -DEMUTLS -DTCR_IN_GPR -I . %~1 m4macros.m4 >  %~1.asm
-    %yasm% %~1.asm -f win64 -g cv8 -p gas -o %~n1.obj
+    %m4% -DWIN_64 -DWINDOWS -DX86 -DX8664 -DHAVE_TLS -DEMUTLS -DTCR_IN_GPR -I . %~n1.s m4macros.m4 >  %~n1.asm
+    %yasm% %~n1.asm -f win64 -g cv8 -p gas -o %~n1.obj
+	del %n1.asm
 ) else (
     echo Usage: m4yasm.bat filename
 )
