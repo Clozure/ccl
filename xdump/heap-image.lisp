3818d471ce0fa307abee0afd8e8fe81612d183b1
Michał "phoe" Herda <phoe@disroot.org>
2019-11-16 11:57:37 +0100

    Refactor heap-image.lisp
    We refactor the file for clarity.
    
    Variables image-sig* have been replaced by a single string variable
    image-file-header that is then used for writing image file headers and
    trailers.
    
    Writing image file headers and trailers has been factored into separate
    functions.

--- xdump/heap-image.lisp
+++ xdump/heap-image.lisp
@@ -15,47 +15,9 @@
 ;;; limitations under the License.
 
 (in-package "CCL")
-(defconstant image-sig0 (dpb (char-code #\O)
-			     (byte 8 24)
-			     (dpb (char-code #\p)
-				  (byte 8 16)
-				  (dpb (char-code #\e)
-				       (byte 8 8)
-				       (char-code #\n)))))
-(defconstant image-sig1 (dpb (char-code #\M)
-			     (byte 8 24)
-			     (dpb (char-code #\C)
-				  (byte 8 16)
-				  (dpb (char-code #\L)
-				       (byte 8 8)
-				       (char-code #\I)))))
-(defconstant image-sig2 (dpb (char-code #\m)
-			     (byte 8 24)
-			     (dpb (char-code #\a)
-				  (byte 8 16)
-				  (dpb (char-code #\g)
-				       (byte 8 8)
-				       (char-code #\e)))))
-(defconstant image-sig3 (dpb (char-code #\F)
-			     (byte 8 24)
-			     (dpb (char-code #\i)
-				  (byte 8 16)
-				  (dpb (char-code #\l)
-				       (byte 8 8)
-				       (char-code #\e)))))
 
-#|
-(def-foreign-type
-    openmcl-image-section-header
-    (:struct nil
-	     (:code :unsigned-long)
-	     (:area (:* t))
-	     (:memory-size :unsigned-long)
-	     (:static-dnodes :unsigned-long)))
-|#
-
-(defun image-write-fullword (w f &optional force-big-endian)
-  (cond ((or force-big-endian *xload-target-big-endian*)
+(defun image-write-u32 (w f)
+  (cond (*xload-target-big-endian*
          (write-byte (ldb (byte 8 24) w) f)
          (write-byte (ldb (byte 8 16) w) f)
          (write-byte (ldb (byte 8 8) w) f)
@@ -66,18 +28,41 @@
          (write-byte (ldb (byte 8 16) w) f)
          (write-byte (ldb (byte 8 24) w) f))))
 
-(defun image-write-doubleword (dw f)
+(defun image-write-u64 (dw f)
   (cond (*xload-target-big-endian*
-         (image-write-fullword (ldb (byte 32 32) dw) f)
-         (image-write-fullword (ldb (byte 32 0) dw) f))
+         (image-write-u32 (ldb (byte 32 32) dw) f)
+         (image-write-u32 (ldb (byte 32 0) dw) f))
         (t
-         (image-write-fullword (ldb (byte 32 0) dw) f)
-         (image-write-fullword (ldb (byte 32 32) dw) f))))
+         (image-write-u32 (ldb (byte 32 0) dw) f)
+         (image-write-u32 (ldb (byte 32 32) dw) f))))
 
 (defun image-write-natural (n f)
   (target-word-size-case
-   (32 (image-write-fullword n f))
-   (64 (image-write-doubleword n f))))
+   (32 (image-write-u32 n f))
+   (64 (image-write-u64 n f))))
+
+(defparameter image-file-header
+  "OpenMCLImageFile")
+
+(defun image-write-string (file string count)
+  (loop for (char-a char-b char-c char-d) on (coerce string 'list) by #'cddddr
+        for i from 0 below count by 4
+        for byte = (dpb (char-code char-a)
+			                   (byte 8 24)
+			                   (dpb (char-code char-b)
+				                      (byte 8 16)
+				                      (dpb (char-code char-c)
+				                           (byte 8 8)
+				                           (char-code char-d))))
+        do (image-write-u32 byte file)))
+
+(defun image-write-file-header (file)
+  (image-write-string file image-file-header 16))
+
+(defun image-write-file-trailer (file header-pos)
+  (image-write-string file image-file-header 12)
+  (let* ((pos (+ 4 (file-position file))))
+    (image-write-u32 (- header-pos pos) file)))
 
 (defun image-align-output-position (f)
   (file-position f (logand (lognot 4095)
@@ -92,58 +77,49 @@
 (defun write-image-file (pathname image-base spaces)
   (let* ((abi-version (target-image-abi-version))
          (image-header-size (* 4 16))
-         (image-section-size (* 4 (target-word-size-case (32 4) (64 8)))))
+         (image-section-size (* 4 (target-word-size-case (32 4) (64 8))))
+         (nsections (length spaces))
+         (header-pos (- 4096 (+ image-header-size (* nsections image-section-size)))))
     (with-open-file (f pathname
                        :direction :output
                        :if-does-not-exist :create
                        :if-exists :supersede
                        :element-type '(unsigned-byte 8))
-      (let* ((nsections (length spaces))
-             (header-pos (- 4096 (+ image-header-size
-                                    (* nsections image-section-size)))))
+      (flet ((write-u32 (x) (image-write-u32 x f))
+             (write-u64 (x) (image-write-u64 x f))
+             (write-nat (x) (image-write-natural x f)))
         (file-position f header-pos)
-        (image-write-fullword image-sig0 f)
-        (image-write-fullword image-sig1 f)
-        (image-write-fullword image-sig2 f)
-        (image-write-fullword image-sig3 f)
-        (image-write-fullword (get-universal-time) f) ;; FIXME: Year 2038 problem.
-        (image-write-fullword (target-word-size-case
-                               (32 *xload-image-base-address*)
-                               (64 0)) f)
-        (image-write-fullword (target-word-size-case
-                               (32 image-base)
-                               (64 0)) f)
-        (image-write-fullword nsections f)
-        (image-write-fullword abi-version f)
-        (target-word-size-case
-         (32
-          (dotimes (i 2) (image-write-fullword 0 f))
-          (image-write-fullword (backend-target-platform *target-backend*) f)
-          (dotimes (i 4) (image-write-fullword 0 f)))
-         (64
-          (image-write-fullword 0 f)
-          (image-write-fullword 0 f)
-          (image-write-fullword (backend-target-platform *target-backend*) f)
-          (image-write-doubleword *xload-image-base-address* f)
-          (image-write-doubleword image-base f)))
+        (image-write-file-header f)
+        (write-u32 (get-universal-time)) ;; FIXME: Year 2038 problem.
+        (write-u32 (target-word-size-case
+                    (32 *xload-image-base-address*)
+                    (64 0)))
+        (write-u32 (target-word-size-case
+                    (32 image-base)
+                    (64 0)))
+        (write-u32 nsections)
+        (write-u32 abi-version)
+        (write-u32 0)
+        (write-u32 0)
+        (write-u32 (backend-target-platform *target-backend*))
+        (write-u64 (target-word-size-case
+                    (32 0)
+                    (64 *xload-image-base-address*)))
+        (write-u64 (target-word-size-case
+                    (32 0)
+                    (64 image-base)))
         (dolist (sect spaces)
-          (image-write-natural (ash (xload-space-code sect)
-                                    *xload-target-fixnumshift*)
-                               f)
-          (image-write-natural 0 f)
+          (write-nat (ash (xload-space-code sect)
+                          *xload-target-fixnumshift*))
+          (write-nat 0)
           (let* ((size (xload-space-lowptr sect)))
-            (image-write-natural size f)
-            (image-write-natural 0 f))) ; static dnodes.
+            (write-nat size)
+            (write-nat 0))) ; static dnodes.
         (dolist (sect spaces)
           (image-align-output-position f)
           (stream-write-ivector f
                                 (xload-space-data sect)
                                 0
                                 (xload-space-lowptr sect)))
-        ;; Write an openmcl_image_file_trailer.
-        (image-write-fullword image-sig0 f)
-        (image-write-fullword image-sig1 f)
-        (image-write-fullword image-sig2 f)
-        (let* ((pos (+ 4 (file-position f))))
-          (image-write-fullword (- header-pos pos) f))
+        (image-write-file-trailer f header-pos)
         nil))))
