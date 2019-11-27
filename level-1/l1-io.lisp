Clozure/ccl

commit 412bd3ee64dc0e4d7964f565789601fb7672a230

Fix ANSI-TEST PRINT-UNREADABLE-OBJECT.2
(print-unreadable-object (nil *standard-output* :type t :identity nil))
incorrectly printed the result as #<NULL> even though the spec page for macro
PRINT-UNREADABLE-OBJECT says:

    If type is true, the output from forms is preceded by a brief description
    of the object's type and a space character.

As much as I think this is an oversight and no spaces should be printed
when both TYPE and IDENTITY are false, specified behaviour is specified
behaviour until we fix the spec itself.

level-1/l1-io.lisp
@@ -599,12 +599,14 @@ printed using \"#:\" syntax.  NIL means no prefix is printed.")
         ((eq stream t) (setq stream *terminal-io*)))
   (write-unreadable-start object stream)
   (when type
-    (princ (type-of object) stream))
-  (when thunk 
-    (when type (stream-write-char stream #\space))
+    (princ (type-of object) stream)
+    (stream-write-char stream #\space))
+  (when thunk
     (funcall thunk))
+  (when (and thunk id)
+    (stream-write-char stream #\space))
   (if id
-    (%write-address object stream #\>)
+    (%write-address object stream (if (and type id) #\> nil))
     (pp-end-block stream ">"))
   nil)
 
@@ -653,10 +655,9 @@ printed using \"#:\" syntax.  NIL means no prefix is printed.")
 (defmethod print-object ((num integer) stream)
   (write-an-integer num stream))
 
-(defun %write-address (object stream &optional foo)
-  (if foo (pp-space stream))
+(defun %write-address (object stream &optional end-block-char)
   (write-an-integer (if (integerp object) object (%address-of object)) stream 16. t)
-  (if foo (pp-end-block stream foo)))
+  (when end-block-char (pp-end-block stream end-block-char)))
 
 (defmethod print-object ((num ratio) stream)
   (let ((base (get-*print-base*)))

commit fbf9b6a4369e49863d6b0b998afb0fb888b8f653

Fix ANSI-TEST PRINT-UNREADABLE-OBJECT.3
Commit 412bd3ee64dc0e4d79 introduced a regression in PRINT-UNREADABLE-OBJECT.3
that caused objects to be printed without the terminating #\>. This commit
fixes that regression.

level-1/l1-io.lisp
@@ -603,10 +603,10 @@ printed using \"#:\" syntax.  NIL means no prefix is printed.")
     (stream-write-char stream #\space))
   (when thunk
     (funcall thunk))
-  (when (and thunk id)
+  (when (or (and thunk id) (and (not type) id))
     (stream-write-char stream #\space))
   (if id
-    (%write-address object stream (if (and type id) #\> nil))
+    (%write-address object stream #\>)
     (pp-end-block stream ">"))
   nil)
 