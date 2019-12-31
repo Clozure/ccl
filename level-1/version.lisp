85f6064a0420e9d06e28417a16c896dec804c7bf
R. Matthew Emerson <rme@acm.org>
2019-12-31 13:27:58 -0800

    Bump version to 1.11.8

--- level-1/version.lisp
+++ level-1/version.lisp
@@ -18,7 +18,7 @@
 
 (defparameter *openmcl-major-version* 1)
 (defparameter *openmcl-minor-version* 11)
-(defparameter *openmcl-revision* 7)
+(defparameter *openmcl-revision* 8)
 ;;; May be set by xload-level-0
 (defvar *openmcl-svn-revision* nil)
 (defparameter *openmcl-dev-level* nil)
