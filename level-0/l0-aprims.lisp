255bc63fd4fb7c2f907f753aa61d7f254e2600dd
Jon Godbout <jgodbou@gmail.com>
2020-05-11 12:13:49 -0400

    Update l0-aprims.lisp

--- level-0/l0-aprims.lisp
+++ level-0/l0-aprims.lisp
@@ -194,7 +194,7 @@ synchronization between threads."
 (defun %make-semaphore-ptr (&key (count 0))
   (let* ((p (ff-call (%kernel-import target::kernel-import-new-semaphore)
 	     :signed-fullword count
-	       :address)))
+             :address)))
     (if (%null-ptr-p p)
       (error "Can't create semaphore.")
       (record-system-lock
