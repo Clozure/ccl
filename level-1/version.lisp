c994217adc56b3f8a564526cee4695893ac84d86
R. Matthew Emerson <rme@acm.org>
2024-08-13 15:06:15 -0700

    Bump to version 1.13

--- level-1/version.lisp
+++ level-1/version.lisp
@@ -17,8 +17,8 @@
 (in-package "CCL")
 
 (defparameter *openmcl-major-version* 1)
-(defparameter *openmcl-minor-version* 12)
-(defparameter *openmcl-revision* 2)
+(defparameter *openmcl-minor-version* 13)
+(defparameter *openmcl-revision* nil)
 ;;; May be set by xload-level-0
 (defvar *openmcl-svn-revision* nil)
 (defparameter *openmcl-dev-level* nil)
