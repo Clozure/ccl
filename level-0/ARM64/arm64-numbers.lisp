b30e007c8bd13c1b42939c94e7e6275dc9a250f9
R. Matthew Emerson <rme@acm.org>
2026-07-09 00:19:54 -0700

    %fixnum-signum and %ilogcount
    The %ilogcount function uses the just-added support for the
    vector arrangement and vector element operads so that we can
    use the cnt and addv instructions to implement popcount.
    
    %fixnum-signum uses condition operands (:? cond) to implement
    signum in a cute, straight-line way.

---
+++ level-0/ARM64/arm64-numbers.lisp
@@ -0,0 +1,20 @@
+;;;;-*- Mode: Lisp; Package: CCL -*-
+;;;;
+;;;; SPDX-License-Identifier: Apache-2.0
+
+(in-package "CCL")
+
+(defarm64lapfunction %fixnum-signum ((number arg_z))
+  (cmp number (:$ 0))
+  (cset imm0 (:? gt))                  ;1 if number > 0, else 0
+  (csinv imm0 imm0 xzr (:? ge))        ;same (0 or 1) if number >= 0, else -1
+  (lsl arg_z imm0 (:$ arm64::fixnumshift))
+  (ret))
+
+(defarm64lapfunction %ilogcount ((number arg_z))
+  (fmov d0 number)
+  (cnt (:8b d0) (:8b d0))
+  (addv (:b d0) (:8b d0))
+  (fmov (:w imm0) (:s d0))
+  (lsl arg_z imm0 (:$ arm64::fixnumshift))
+  (ret))
