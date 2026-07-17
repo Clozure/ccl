f5443fab2ecf2b535a87aeddbace598792a2dd21
Mauro DiBenedetto <maurodibenedetto@gmail.com>
2026-07-17 14:00:00 -0400

    xfasload fasl function fulltag
    Signed-off-by: Mauro DiBenedetto <maurodibenedetto@gmail.com>

--- xdump/xfasload.lisp
+++ xdump/xfasload.lisp
@@ -340,7 +340,10 @@
 
 
 (defun  %xload-unbound-function% ()
-  (+ *xload-dynamic-space-address* *xload-target-fulltag-misc*))
+  (+ *xload-dynamic-space-address*
+     (target-arch-case
+      (:arm64 *xload-target-fulltag-for-functions*)
+      (otherwise *xload-target-fulltag-misc*))))
 
 (defparameter *xload-dynamic-space* nil)
 (defparameter *xload-readonly-space* nil)
@@ -1059,6 +1062,17 @@
                   *xload-target-backend*)))
           (locally (declare (ftype (function (t) t) xload-arm-set-entrypoint))
             (xload-arm-set-entrypoint udf-object))))
+       (:arm64
+        ;; arm64 gives functions their own fulltag; callers load the
+        ;; code vector from slot 0 of a function-tagged pointer, so the
+        ;; PPC simple-vector pun (misc tag) misreads.  Real function
+        ;; object: {code-vector@0, lfbits@1}.
+        (let* ((udf-object (xload-make-gvector :function 2)))
+          (setf (xload-%svref udf-object 0)
+                (xload-save-code-vector
+                 (backend-xload-info-udf-code
+                  *xload-target-backend*)))
+          (setf (xload-%svref udf-object 1) 0)))
        (otherwise
         ;; The undefined-function object is a 1-element simple-vector (not
         ;; a function vector).  The code-vector in its 0th element should
@@ -1463,9 +1477,13 @@
 (defxloadfaslop $fasl-symfn (s)
   (let* ((symaddr (%fasl-expr-preserve-epush s))
          (fnobj (xload-%svref symaddr target::symbol.fcell-cell)))
-    (if (and (= *xload-target-fulltag-misc*
+    (if (and (= *xload-target-fulltag-for-functions*
                 (logand fnobj *xload-target-fulltagmask*))
-             (= (type-keyword-code :function) (xload-u8-at-address (+ fnobj *xload-target-misc-subtag-offset*))))
+             (= (type-keyword-code :function)
+                (xload-u8-at-address
+                 (+ (logior *xload-target-fulltag-misc*
+                            (logandc2 fnobj *xload-target-fulltagmask*))
+                    *xload-target-misc-subtag-offset*))))
       (%epushval s fnobj)
       (error "symbol at #x~x is unfbound . " symaddr))))
 
@@ -1646,7 +1664,28 @@
   (xfasl-read-gvector s (xload-target-subtype :simple-vector)))
 
 (defxloadfaslop $fasl-function (s)
-  (xfasl-read-gvector s (xload-target-subtype :function)))
+  ;; On a target whose function tag is a FULLTAG rather than a subtag of
+  ;; fulltag-misc, the pushed/returned value must carry that tag --
+  ;; self-references read during element fill must see the final tag.
+  ;; $fasl-clfun applies the same rule.  x8664 is the only such target
+  ;; (x8664-arch.lisp:349); arm64 is NOT one -- since the fulltag_function
+  ;; removal (patch 0055) an arm64 function is fulltag-misc + header
+  ;; subtag-function, and *xload-target-fulltag-for-functions* derives
+  ;; from :function-tag-is-subtag, so it equals fulltag-misc there.  The
+  ;; form is kept because it is the general one and reduces to the
+  ;; previous behavior on every subtag target.
+  (let* ((n (%fasl-read-count s))
+         (vector (xload-make-gvector (xload-target-subtype :function) n))
+         (function (logior *xload-target-fulltag-for-functions*
+                           (logandc2 vector *xload-target-fulltagmask*))))
+    (%epushval s function)
+    (dotimes (i n)
+      (setf (xload-%svref vector i) (%fasl-expr s)))
+    (target-arch-case
+     (:arm
+      (locally (declare (ftype (function (t) t) xload-arm-set-entrypoint))
+        (xload-arm-set-entrypoint vector))))
+    (setf (faslstate.faslval s) function)))
 
 (defxloadfaslop $fasl-istruct (s)
   (xfasl-read-gvector s (xload-target-subtype :istruct)))
