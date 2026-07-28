1b9b79ba6f681004cbbd03954f1278727049f9fe
R. Matthew Emerson <rme@acm.org>
2026-07-27 22:09:13 -0700

    Guess at arm64 conditionalization in context-for-suspended-tcr

--- lib/backtrace.lisp
+++ lib/backtrace.lisp
@@ -22,6 +22,7 @@
 #+ppc-target (require "PPC-BACKTRACE")
 #+x86-target (require "X86-BACKTRACE")
 #+arm-target (require "ARM-BACKTRACE")
+#+arm64-target (require "ARM64-BACKTRACE")
 
 
 (defparameter *backtrace-show-internal-frames* nil)
@@ -42,7 +43,7 @@
                         nil       ;; condition - not used
                         frame-ptr ;; current
                         #+ppc-target *fake-stack-frames*
-                        #+(or x86-target arm-target) frame-ptr
+                        #+(or x86-target arm-target arm64-target) frame-ptr
                         (%fixnum-ref tcr (- target::tcr.db-link
 					    target::tcr-bias))
                         0         ;; break level - not used
