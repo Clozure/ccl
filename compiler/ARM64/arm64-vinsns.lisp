62c5cdc858207e91dd28a0638583068960424aad
R. Matthew Emerson <rme@acm.org>
2026-09-11 11:52:02 -0700

    Revert "arm64: use FMA instructions in complex multiply vinsns"
    This reverts commit 4bbe073a94c3b611c9083f43339b50a94153a8ab.
    
    Stas Boukarev pointed out that the fewer roundings done with FMA
    ops can cause the specialized and generic complex-* results to differ.
    
    Concretely, this means that this would change
    
    R(R(xr*yi) + R(xi*yr))
    
    to
    
    R(xr*yi + R(xi*yr))
    
    where R is rounding.
    
    This means that we'll get different answers depending on declarations,
    which seems bad.
    
    If we want to exploit this ISA feature, it would be better to put
    it behind some kind of compiler policy settings.

--- compiler/ARM64/arm64-vinsns.lisp
+++ compiler/ARM64/arm64-vinsns.lisp
@@ -7619,10 +7619,12 @@
                                               (yi :double-float)))
   (dup xi (:d x 1))
   (dup yi (:d y 1))
+  (fmul rr (:d x) (:d y))               ;xr*yr
   (fmul ri xi yi)                       ;xi*yi
-  (fnmsub rr (:d x) (:d y) ri)          ;realpart = xr*yr - xi*yi
+  (fsub rr rr ri)                       ;realpart
   (fmul xi xi (:d y))                   ;xi*yr
-  (fmadd ri (:d x) yi xi)               ;imagpart = xr*yi + xi*yr
+  (fmul yi yi (:d x))                   ;yi*xr
+  (fadd ri xi yi)                       ;imagpart
   (fmov (:d result) rr)
   (ins (:d result 1) (:d ri 0)))
 
@@ -7645,10 +7647,12 @@
                                               (yi :single-float)))
   (dup xi (:s x 1))
   (dup yi (:s y 1))
+  (fmul rr (:s x) (:s y))               ;xr*yr
   (fmul ri xi yi)                       ;xi*yi
-  (fnmsub rr (:s x) (:s y) ri)          ;realpart = xr*yr - xi*yi
+  (fsub rr rr ri)                       ;realpart
   (fmul xi xi (:s y))                   ;xi*yr
-  (fmadd ri (:s x) yi xi)               ;imagpart = xr*yi + xi*yr
+  (fmul yi yi (:s x))                   ;yi*xr
+  (fadd ri xi yi)                       ;imagpart
   (fmov (:s result) rr)
   (ins (:s result 1) (:s ri 0)))
 
